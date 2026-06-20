be7220a21672cd4658b39533e4ae4ce488eacbe4
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-06-20 16:28:29 +0530

    Ai recommendation service setup

---
+++ src/main/java/com/example/storyloom_ai_recommendation_service/StoryloomAiRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.storyloom_ai_recommendation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryloomAiRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryloomAiRecommendationServiceApplication.class, args);
+	}
+
+}
