487f5a26192f44b1f8ed56444c96f3a292c23e15
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-07-05 17:05:19 +0530

    Added docker configuration

--- src/main/java/com/example/storyloom_ai_recommendation_service/externalInterfaces/StoryloomCatalogService.java
+++ src/main/java/com/example/storyloom_ai_recommendation_service/externalInterfaces/StoryloomCatalogService.java
@@ -2,13 +2,11 @@ package com.example.storyloom_ai_recommendation_service.externalInterfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Map;
+
 
 @FeignClient("STORYLOOM-CATALOG-SERVICE")
 public interface StoryloomCatalogService {
